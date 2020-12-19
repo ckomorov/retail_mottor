ca00d9980d311061c6b113d00db683414950e4ae
Sergey <ckomorov1@gmail.com>
2020-12-19 15:16:24 +0300

    create order in retail

--- app/RequestController.php
+++ app/RequestController.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Monolog\Logger;
+use RetailCrm\ApiClient;
+use RetailCrm\Exception\CurlException;
 
 class RequestController
 {
@@ -12,18 +14,60 @@ class RequestController
     /** @var Logger */
     public $logger;
 
-    public function __construct(array $request, Logger $logger)
+    /** @var ApiClient  */
+    public $client;
+
+    public function __construct(array $request, ApiClient $client, Logger $logger)
     {
         $this->request = $request;
+        $this->client = $client;
         $this->logger = $logger;
     }
 
     public function parseRequest(): array
     {
-        $result = $this->request;
+        $data = $this->request;
+        $this->logger->info("New Request from LpMotor: ", [$this->request]);
 
-        $this->logger->info(date("Y-m-d H:i:s: "), [json_encode($this->request)]);
+        try {
+            $newOrder = $this->createOrderInRetail($data, $this->client);
+        } catch (\Exception $e) {
 
-        return $result;
+        }
+
+        return $newOrder ?? [];
     }
+
+    private function createOrderInRetail(array $request, ApiClient $client): array
+    {
+        try {
+            $response = $client->request->ordersCreate(array(
+                'externalId' => $request['id_lead'],
+                'number' => $request['id_lead'],
+                'firstName' => $request['name'] ?? 'no name',
+                'email' => $request['email'] ?? '',
+                'phone' => $request['phone'],
+                'customerComment' => $request['frm_title'],
+            ));
+        } catch (\RetailCrm\Exception\CurlException $e) {
+            $this->logger->error(
+                date("Y-m-d H:i:s: ") . "Connection error: ",
+                [json_encode($e->getMessage(), JSON_UNESCAPED_UNICODE)]
+            );
+        }
+
+        if ($response->isSuccessful() && 201 === $response->getStatusCode()) {
+            $this->logger->info(
+                'Order successfully created.',
+                ['Order ID into RetailCRM = ' . $response->id]
+            );
+        } else {
+            $this->logger->error(
+                sprintf("Error: [HTTP-code %s]", $response->getStatusCode()),
+                [$response['errors'] ? $response['errors']: 'Response has no errors']
+            );
+        }
+        return [];
+    }
+
 }
